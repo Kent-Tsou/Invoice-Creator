aaccc71269253519cf3669f28a198e91c23e9cee
Kent Tsou <tsoukent97@gmail.com>
2022-03-21 15:00:29 +1300

    updated html file and index.js with pseudocode

---
+++ index.js
@@ -0,0 +1,23 @@
+const btn_1 = document.getElementById("btn-1")
+const btn_2 = document.getElementById("btn-2")
+const btn_3 = document.getElementById("btn-3")
+const job_Title = document.getElementById("job-title")
+const job_Price = document.getElementById("job-price")
+const sum_el = document.getElementById("sum-el")
+const send_Btn = document.getElementById("send-Btn")
+
+// addEventListener to buttons 1, 2, 3, and send button
+btn_1.addEventListener("click", function() {
+
+})
+
+btn_2.addEventListener("click", function() {
+    
+})
+
+btn_3.addEventListener("click", function() {
+    
+})
+// when we press buttons we want task and total to render 
+// (job_Title and job_Price)
+// we also want total amount to render the total (sum_el)
