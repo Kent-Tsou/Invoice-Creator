0ce276c11ab288bb4210cae3ad0fc116165264b9
Kent Tsou <tsoukent97@gmail.com>
2022-03-30 17:12:21 +1300

    job1 button update

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-const btn_1 = document.getElementById("btn-1")
-const btn_2 = document.getElementById("btn-2")
-const btn_3 = document.getElementById("btn-3")
+const job1Arr = [["Wash Car",10]];
+const job2Arr = [["Mow Lawn",20]];
+const job3Arr = [["Pull Weed",30]];
+let jobs = [];
+const job_1 = document.getElementById("job-1")
+const job_2 = document.getElementById("job-2")
+const job_3 = document.getElementById("job-3")
 const job_Title = document.getElementById("job-title")
 const job_Price = document.getElementById("job-price")
 const sum_el = document.getElementById("sum-el")
 const send_Btn = document.getElementById("send-Btn")
+let totalPrice = 0;
 
 // addEventListener to buttons 1, 2, 3, and send button
-btn_1.addEventListener("click", function() {
-
+job_1.addEventListener("click", function() {
+    if(!jobs.includes(job1Arr)){
+        let titleName = document.createElement('p');
+        let priceName = document.createElement('p');
+        let priceNumber = document.createElement('span');
+        titleName.textContent = job1Arr[0][0];
+        priceName.textContent = "$";
+        priceNumber.classList.add('price');
+        priceNumber.textContent = job1Arr[0][1];
+        priceName.append(priceNumber)
+        job_Title.appendChild(titleName);
+        job_Price.appendChild(priceName);
+        jobs.push(job1Arr);
+        totalPrice += job1Arr[0][1];
+        ammount.textContent = "$" + totalPrice;
+    }else{
+        alert("Already Added!");
+    }
 })
 
-btn_2.addEventListener("click", function() {
+job_2.addEventListener("click", function() {
     
 })
 
-btn_3.addEventListener("click", function() {
+job_3.addEventListener("click", function() {
     
 })
 // when we press buttons we want task and total to render 
